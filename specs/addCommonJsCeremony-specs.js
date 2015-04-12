fb3706393ab54471b7ccd68d4e2b86f0f990a48a
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-04-12 13:09:53 -0400

    Removes ANSI escape codes. Extracts Steps wraps them into a commonJS module.

---
+++ specs/addCommonJsCeremony-specs.js
@@ -0,0 +1,11 @@
+var chai = require('chai'),
+    pepo = require('../lib/pepo');
+
+describe('Adding CommonJS export ceremony', function () {
+  before(function () {
+    chai.should();
+  });
+  it('should wrap any input within a function and export it.', function () {
+    pepo.addCommonJSCeremony('').should.eql('module.exports = function () {\n\n};');
+  });
+});
--- specs/extractSteps-specs.js
+++ specs/extractSteps-specs.js
@@ -1,9 +1,17 @@
-var chai    = require('chai'),
-    pepo    = require('../lib/pepo');
+var chai = require('chai'),
+    pepo = require('../lib/pepo'),
+    fs   = require('fs');
 
-describe('Handling ANSI escape codes', function () {
+describe('Extracting individual steps.', function () {
   before(function () {
     chai.should();
+  });
+
+  it('should match all steps in sample file.', function (done) {
+    fs.readFile('specs/samples/clean.sample', function (err, sample) {
+      pepo.extractSteps(sample).replace(/this\.(Given|When|Then)(.|\s)+?\}\)\;\n/g,'').should.match(/[\s]+?/);
+      done();
+    });
   });
 
 });
