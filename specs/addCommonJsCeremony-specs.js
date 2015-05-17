1dd74f80cf345e01ee2331c8e6e482d17ad688e6
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-05-17 18:34:13 -0400

    added support for the wrap option in #addCommonJSCeremony

--- specs/addCommonJsCeremony-specs.js
+++ specs/addCommonJsCeremony-specs.js
@@ -5,7 +5,12 @@ describe('Adding CommonJS export ceremony', function () {
   before(function () {
     chai.should();
   });
+  
   it('should wrap any input within a function and export it.', function () {
     pepo.addCommonJSCeremony('').should.eql('module.exports = function () {\n\n};');
   });
+  
+  it('should NOT touch the input if the wrap option is disabled.', function () {
+    pepo.addCommonJSCeremony('',{ wrap: false }).should.eql('');
+  }); 
 });
